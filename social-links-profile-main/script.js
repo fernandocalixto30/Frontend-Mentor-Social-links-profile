7328bf57d1ba19ae0f4b2195d76727778179eabe
FERNANDO CALIXTO <107202032+fernandocalixto30@users.noreply.github.com>
2024-04-29 11:32:38 -0300

    Delete social-links-profile-main directory

--- social-links-profile-main/script.js
+++
@@ -1,9 +0,0 @@
-
-document.addEventListener('DOMContentLoaded', function () {
-    var evento1 = document.getElementById('evento1');
-    var evento = document.getElementById('evento');
-    evento.addEventListener('click', function () {
-    
-        colorButton.style.backgroundColor = randomColor;
-});
-});
